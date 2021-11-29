e6fc8c854c828cc7c7c212ac01d9ce27a8b8e266
George-T-Serban <georgeitprojects@gmail.com>
2021-11-28 16:33:50 -0800

    added main vpc

---
+++ variables.tf
@@ -0,0 +1,57 @@
+variable "aws_region" {
+    description = "AWS Region"
+    type = string
+    default = "us-east-1"
+}
+
+variable "vpc_name" {
+    description = "VPC name"
+    type = string
+    default = "wordpress_vpc"
+    }
+
+variable "vpc_tags" {
+    description = "VPC tags"
+    type = map(string)
+    default = {
+        Terraform = "true"
+        Environment = "wordpress-demo"
+    }
+}
+
+variable "vpc_azs" {
+    description = "Availability zones"
+    type = list(string)
+    default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "vpc_cidr" {
+    description = "VPC CIDR Block"
+    type = string
+    default = "10.16.0.0/16"
+}
+
+variable "public_subnet_cidr_blocks" {
+    description = "Available cidr blocks for public subnets"
+    type = list(string)
+    default = ["10.16.48.0/20", "10.16.112.0/20", "10.16.176.0/20"]
+}
+
+variable "private_subnet_cidr_blocks" {
+    description = "Available cidr blocks for private subnets"
+    type = list(string)
+    default = [
+        "10.16.16.0/20",
+        "10.16.80.0/20",
+        "10.16.144.0/20",
+        "10.16.32.0/20",
+        "10.16.96.0/20",
+        "10.16.160.0/20"
+    ]
+}
+
+
+
+
+
+
---
+++ vpc.tf
@@ -0,0 +1,20 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.11.0"
+
+name = var.vpc_name
+cidr = var.vpc_cidr
+
+azs = var.vpc_azs
+private_subnets = var.private_subnet_cidr_blocks
+public_subnets = var.public_subnet_cidr_blocks
+
+enable_nat_gateway = true
+single_nat_gateway = true
+one_nat_gateway_per_az =  false
+create_igw = true
+manage_default_route_table = true 
+enable_vpn_gateway = false
+
+
+}
